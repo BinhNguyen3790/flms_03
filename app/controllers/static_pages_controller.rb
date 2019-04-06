5a06dd8224e9c5f78623168bde622c5582c1ff71
BinhNguyen3790 <nguyenvanbinh3790@gmail.com>
2019-04-06 14:48:05 +0700

    Search book in home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
-    @books = Book.search_book(params[:search]).newest.paginate page: params[:page],
-      per_page: Settings.static_page.per_page
+    @books =
+      Book.search_book(params[:search]).newest.paginate page: params[:page],
+        per_page: Settings.static_page.per_page
   end
 
   def help; end
