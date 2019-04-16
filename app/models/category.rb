c27bea3af14cb7c43c7e3473663745596fd3b58f
BinhNguyen3790 <nguyenvanbinh3790@gmail.com>
2019-04-16 09:47:22 +0700

    view list categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
   has_many :books, dependent: :destroy
   validates :name, presence: true, length: {maximum: Settings.category.name}
+  scope :newest, ->{order created_at: :desc}
 end
